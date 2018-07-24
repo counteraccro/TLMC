3aabd3a580ffaeab2789ecaebfcf36a9e39c8cdc
Aymeric <counteraccro@gmail.com>
2018-07-24 16:41:14 +0200

    Ajout des commentaires dans FormAddCollection.js

--- public/assets/js/utils/FormAddCollection.js
+++ public/assets/js/utils/FormAddCollection.js
@@ -1,28 +1,33 @@
 FormAddCollection = {};
 
 FormAddCollection.Launch = function(params) {
-
+	
+	//id du conteneur
 	FormAddCollection.container = $(params.id_container);
+	//id du bouton d'ajout
 	FormAddCollection.id_btn_add = params.id_btn_add;
+	//id du titre du formulaire imbriqué
 	FormAddCollection.id_label_fieldset = params.id_label_fieldset;
+	//id du bouton de suppression
 	FormAddCollection.btn_delete = params.btn_delete;
 
 	// On définit un compteur unique pour nommer les champs qu'on va ajouter dynamiquement
 	FormAddCollection.index = FormAddCollection.container.find(':input').length;
 
 	FormAddCollection.Event = function() {
-
+		
+		// Ajout d'un élément au clic
 		$(FormAddCollection.id_btn_add).click(function(e) {
 			FormAddCollection.AddElement();
 
-			e.preventDefault();
+			e.preventDefault(); // évite qu'un # apparaisse dans l'URL
 			return false;
 		});
 
 		if (FormAddCollection.index == 0) {
 			FormAddCollection.AddElement();
 		} else {
-			// S'il existe déjà des catégories, on ajoute un lien de suppression pour chacune d'entre elles
+			// S'il existe déjà des éléments, on ajoute un lien de suppression pour chacun d'entre eux
 			FormAddCollection.container.children('div').each(function() {
 				FormAddCollection.addDeleteLink($(this));
 			});
@@ -30,8 +35,9 @@ FormAddCollection.Launch = function(params) {
 		}
 	}
 
+	//ajout d'un élément dans le conteneur
 	FormAddCollection.AddElement = function() {
-
+		
 		$(FormAddCollection.id_label_fieldset).hide();
 
 		var template = FormAddCollection.container.attr('data-prototype');
@@ -39,7 +45,7 @@ FormAddCollection.Launch = function(params) {
 		// On crée un objet jquery qui contient ce template
 		var prototype = $(template);
 
-		// On ajoute au prototype un lien pour pouvoir supprimer la catégorie
+		// On ajoute au prototype un lien pour pouvoir supprimer l'élément
 		FormAddCollection.AddDeleteLink(prototype);
 
 		// On ajoute le prototype modifié à la fin de la balise <div>
@@ -48,7 +54,8 @@ FormAddCollection.Launch = function(params) {
 		// Enfin, on incrémente le compteur pour que le prochain ajout se fasse avec un autre numéro
 		FormAddCollection.index++;
 	}
-
+	
+	//création d'un lien de suppression d'un élément appartenant au conteneur
 	FormAddCollection.AddDeleteLink = function(prototype) {
 		// Création du lien
 		var deleteLink = $(FormAddCollection.btn_delete);
@@ -56,13 +63,13 @@ FormAddCollection.Launch = function(params) {
 		// Ajout du lien
 		prototype.append(deleteLink);
 
-		// Ajout du listener sur le clic du lien pour effectivement supprimer la catégorie
+		// Ajout du listener sur le clic du lien pour effectivement supprimer l'élément
 		deleteLink.click(function(e) {
 
 			$(FormAddCollection.id_label_fieldset).show();
 
 			prototype.remove();
-			e.preventDefault(); // évite qu'un # apparaisse dans l'URL
+			e.preventDefault();
 			return false;
 		});
 	}
