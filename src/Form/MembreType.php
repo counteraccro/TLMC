d7cee29e2ca7fdfe94e8f8fcc0a5dbc4bd9b4038
Christine HENRIOT <christine.henriot2@alten.com>
2018-07-31 15:51:07 +0200

    Ajout de la spécialité dans le formulaire du membre
    Le champ spécialité dépend de l'établissement sélectionné

--- src/Form/MembreType.php
+++ src/Form/MembreType.php
@@ -11,8 +11,10 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use App\Entity\Etablissement;
+use App\Entity\Specialite;
 
 class MembreType extends AbstractType
 {
@@ -28,14 +30,20 @@ class MembreType extends AbstractType
         ))
             ->add('password', RepeatedType::class, array(
             'first_options' => array(
+                'always_empty' => false,
                 'label' => 'Mot de passe'
             ),
             'second_options' => array(
+                'always_empty' => false,
                 'label' => 'Confirmation du mot de passe'
             ),
+            'type' => PasswordType::class,
+            'required' => false,
+
             'invalid_message' => 'Les mots de passe doivent correspondre',
             'options' => array(
                 'attr' => array(
+
                     'class' => 'password-field'
                 )
             )
@@ -56,12 +64,15 @@ class MembreType extends AbstractType
             'required' => false
         ))
             ->add('signature', TextareaType::class)
-            ->
-        // ->add('roles')
-        // ->add('specialite')
-        add('etablissement', EntityType::class, array(
+            ->add('etablissement', EntityType::class, array(
             'class' => Etablissement::class,
             'choice_label' => 'nom'
+        ))
+            ->add('specialite', EntityType::class, array(
+            'class' => Specialite::class,
+            'choice_label' => 'service',
+            'label' => 'Spécialité',
+            'required' => false
         ))
             ->add('save', SubmitType::class, array(
             'label' => $options['label_submit'],
