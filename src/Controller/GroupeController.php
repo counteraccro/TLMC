5543f244af975eafbfd8e34a20eb98c87bc807d9
Ingrid <ingrid.weil@coding-academy.fr>
2018-09-04 20:47:46 +0200

    Avancement Groupe controller + repo (listeGroupesByUser)
    controller groupe/ajax/listegroupes
    début fonction findByUser dans repo Groupe (SQL)

--- src/Controller/GroupeController.php
+++ src/Controller/GroupeController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use App\Entity\Groupe;
 
 class GroupeController extends AppController
@@ -21,4 +24,21 @@ class GroupeController extends AppController
             'groupes' => $groupes
         ]);
     }
+    
+     /**
+     * Récupère la liste des groupes de l'utilisateur courant
+     *
+     * @Route("/groupe/ajax/listegroupes", name="groupe_ajax_listegroupes")
+     * @Security("is_granted('ROLE_ADMIN') or is_granted('ROLE_BENEVOLE') or is_granted('ROLE_BENEFICIAIRE') or is_granted('ROLE_BENEFICIAIRE_DIRECT')")
+     */
+    public function ajaxListeGroupesAction()
+    {
+        $repository = $this->getDoctrine()->getRepository(Groupe::class);
+        $listeGroupes = $repository->findByUser($this->getUser()->getId());
+        
+        return $this->json(array(
+            'listeGroupes' => $listeGroupes
+        ));
+    }
 }
+   
--- src/Repository/GroupeRepository.php
+++ src/Repository/GroupeRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\Groupe;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\ORM\Query\Expr;
 
 /**
  * @method Groupe|null find($id, $lockMode = null, $lockVersion = null)
@@ -17,6 +18,25 @@ class GroupeRepository extends ServiceEntityRepository
     public function __construct(RegistryInterface $registry)
     {
         parent::__construct($registry, Groupe::class);
+    }
+    
+    /**
+     * Fonction permettant de récupérer les groupes du membre courant
+     */
+    public function findByUser($id_membre)
+    {   
+        $query = $this->createQueryBuilder('g')
+        ->select('COUNT(DISTINCT g.id)');
+        /*SELECT DISTINCT groupe.id, groupe.nom, groupe_membre.membre_id, membre.prenom, membre.nom
+         * FROM groupe join groupe_membre ON groupe_membre.groupe_id = groupe.id
+         * JOIN membre ON membre.id = groupe_membre.membre_id
+         * WHERE membre.id = 7*/
+
+        $result = $query->getQuery()->getSingleScalarResult();
+        
+        return array(
+            'result' => $result
+        );
     }
 
 //    /**
